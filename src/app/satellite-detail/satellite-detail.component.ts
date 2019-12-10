f0b840153e183ba8a00adeab8499128e6ec01a74
Michael McAllister <michaelmcallister@Michaels-MBP.fios-router.home>
2019-12-10 14:01:13 -0500

    satellite details working

--- src/app/satellite-detail/satellite-detail.component.ts
+++ src/app/satellite-detail/satellite-detail.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { SatelliteService } from '../satellite.service';
+import { MapService } from '../map.service';
 @Component({
   selector: 'app-satellite-detail',
   templateUrl: './satellite-detail.component.html',
   styleUrls: ['./satellite-detail.component.css']
 })
 export class SatelliteDetailComponent implements OnInit {
+  satellite;
+  markers;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private satelliteService: SatelliteService,
+    private mapService: MapService
+  ) { }
 
   ngOnInit() {
-  }
 
+    this.markers= this.mapService.getMarkers();
+
+    this.route.paramMap.subscribe(params => {
+      this.satellite = this.markers.find(sat => sat.satid == parseInt(params.get('satelliteId')));
+      console.log(this.satellite); 
+    });
+
+    
+
+    //this.getSatellites(this.category.id);
+  }
+//satelliteId
 }
