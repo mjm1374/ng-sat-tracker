49a9368017960ba392e073a72fe2d73552250da9
Mike McAllister <mike@logikbox.com>
2020-12-29 17:05:23 -0500

    app running again

--- src/app/satellites.service.spec.ts
+++ src/app/satellites.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject, waitForAsync } from '@angular/core/testing';
 import { SatelliteService } from './satellite.service';
 
 describe('Service: Satelites', () => {
